67ce098d326551e8db3608e5a453dbea491a9e73
xzj <783638806@qq.com>
2023-09-01 14:19:55 +0800

    1

---
+++ 线程学习/ConsoleApplication1/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <thread>
+
+void counter(int id, int numIerateions)
+{
+	for (int i{ 0 }; i < numIerateions; ++i)
+	{
+		std::cout << "Counter" << id << "has value" << i << std::endl;
+	}
+}
+
+void test1()
+{
+	std::thread t1 {counter, 1, 6};
+	std::thread t2 {counter, 2, 6};
+	t1.join();
+	t2.join();
+}
+
+int main()
+{
+	test1();//通过函数指针创建线程
+}
